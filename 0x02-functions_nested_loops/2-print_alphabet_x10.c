529e508bcb144aeb8b294bb9de8cde94070121a6
WillElvire <elvirekoua2000@gmail.com>
2022-03-15 13:50:19 -0700

    commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,11 @@
 # include "main.h"
 
 /**
- * print_alphabet - Prints the alphabets in lower case
+ * print_alphabet_x10 - Prints the alphabets in lower case
  *
  * Return: none
  */
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
 	char a = 'a', z = 'z';
 	int i = 0;
